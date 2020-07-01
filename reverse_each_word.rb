2476acd4ee2695a88c8e751f92b8558d66351c1a
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-07-01 03:42:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,6 @@ def reverse_each_word(string)
   array = string.split()
   new_array = []
   array.collect do |word|
-    new_array = word.reverse
+    word.reverse.join(' ')
   end
-  new_array.join(' ')
 end
